c7774728307ae7cabaf28856eb7d094d67bd06e0
Yjaden Christopher Wood <ycw942@clayface.ece.northwestern.edu>
2022-03-16 14:23:43 -0500

    Final Commit!

--- run_model.py
+++ run_model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pickle
+import matplotlib.pyplot as plt
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
@@ -8,100 +10,18 @@ import numpy as np
 import pdb
 from data.my_dataset import *
 from models import *
-#import cProfile
-import pickle
-import math
-import gc
-import time
-def run_model(model, running_mode='train', train_set=None, valid_set=None, test_set=None,
-              batch_size=1, learning_rate=0.01, n_epochs=1, stop_thr=1e-4, shuffle=True):
-
-    """
-        model: 
-        running_mode:
-        train_set, valid_set, test_set:
-        batch_size:
-        learning_rate:
-        n_epochs:
-        stop_thr:
-        shuffle:
-
-    This function either trains or evaluates a model.
-
-    training mode: the model is trained and evaluated on a validation set, if provided.
-                   If no validation set is provided, the training is performed for a fixed
-                   number of epochs.
-                   Otherwise, the model should be evaluated on the validation set
-                   at the end of each epoch and the training should be stopped based on one
-                   of these two conditions (whichever happens first):
-                   1. The validation loss stops improving.
-                   2. The maximum number of epochs is reached.
-
-    testing mode: the trained model is evaluated on the testing set
-
-    Inputs: 
-
-    model: the neural network to be trained or evaluated
-    running_mode: string, 'train' or 'test'
-    train_set: the training dataset object generated using the class MyDataset 
-    valid_set: the validation dataset object generated using the class MyDataset
-    test_set: the testing dataset object generated using the class MyDataset
-    batch_size: number of training samples fed to the model at each training step
-    learning_rate: determines the step size in moving towards a local minimum
-    n_epochs: maximum number of epoch for training the model 
-    stop_thr: if the validation loss from one epoch to the next is less than this
-              value, stop training
-    shuffle: determines if the shuffle property of the DataLoader is on/off
-
-    Outputs when running_mode == 'train':
-
-    model: the trained model 
-    loss: dictionary with keys 'train' and 'valid'
-          The value of each key is a list of loss values. Each loss value is the average
-          of training/validation loss over one epoch.
-          If the validation set is not provided just return an empty list.
-    acc: dictionary with keys 'train' and 'valid'
-         The value of each key is a list of accuracies (percentage of correctly classified
-         samples in the dataset). Each accuracy value is the average of training/validation
-         accuracies over one epoch.
-         If the validation set is not provided just return an empty list.
-
-    Outputs when running_mode == 'test':
-
-    loss: the average loss value over the testing set. 
-    accuracy: percentage of correctly classified samples in the testing set. 
-
-    Summary of the operations this function should perform:
-    1. Use the DataLoader class to generate training, validation, or test data loaders
-    2. In the training mode:
-       - define an optimizer (we use SGD in this homework)
-       - call the train function (see below) for a number of epochs until a stopping
-         criterion is met
-       - call the test function (see below) with the validation data loader at each epoch
-         if the validation set is provided
-
-    3. In the testing mode:
-       - call the test function (see below) with the test data loader and return the results
-    """
-        
-    # send the model to the GPU here
-
-    pass
-
-
-def _train(model, data_loader, optimizer, device=torch.device("cuda:0")):
+   
+def _train(model, data_loader, valid_loader, optimizer, device=torch.device("cuda:0")):
     """
     This function will implement one epoch of training a given model
-
     Inputs:
     model: the neural network to be trained
     data_loader: for loading the network input and targets from the training dataset
     optimizer: the optimiztion method, e.g., SGD
     device: hanlon's GPU
-
     Outputs:
     model: the trained model
-    
+
     """
     print("\nTRAINING MODEL\n")
     # Set Loss Func
@@ -109,6 +29,8 @@ def _train(model, data_loader, optimizer, device=torch.device("cuda:0")):
     # Set Losses Numpy Array
     losses = torch.zeros(len(data_loader), dtype=torch.float64, device=device)#np.empty(shape=len(data_loader), dtype='f') # CHANGE: Numpy array to hold losses
     large_losses = 0
+    training_perp = torch.zeros(20, dtype=torch.float64, device=device)
+    valid_perp = torch.zeros(20, dtype=torch.float64, device=device)
     # Set Epoch Timer
     epoch_timer = time.time()
     # Set Count
@@ -119,47 +41,35 @@ def _train(model, data_loader, optimizer, device=torch.device("cuda:0")):
         model_input_tensor = torch.zeros((20, 5), device=device).long() # Tensor For Inputs
         final_word_indices = torch.zeros(20, device=device).long()      # Tensor For Idxs Target Token
         y_target = torch.zeros(1, len(data_loader.vocab), device=device).long() # Tensor to hold target probability distrubtion'
-        for i, data in enumerate(data_loader): 
+
+        for i, data in enumerate(data_loader):
             # Batch Idx
             batch_idx = i % 20
             # Get context words, ground truth
-            context, final_word = data 
+            context, final_word = data
+
             # Loop through context and build input tensor
             for idx, word in enumerate(context):
                 # Lookup index for token and add to input tensor
                 model_input_tensor[batch_idx][idx] = data_loader.vocab2index[word]
-            # Lookup index for ground truth  
+            # Lookup index for ground truth
             final_word_indices[batch_idx] = data_loader.vocab2index[final_word]
             # Timer for batch
             batch_timer = time.time()
+
             # Check if we've met our batch size
             if i % 20 == 0 and i != 0:
-                # Zero Grads 
+                # Zero Grads
                 # If so, get ready to pass input to model
                 for batch_input_count in range(20):
-                    # Get Model Prediction  
+                    # Get Model Prediction
                     output_logits, log_probs = model(model_input_tensor[batch_input_count])
                     #log_probs = torch.reshape(output_logits, (1, len(data_loader.vocab))) # Reshape for CrossEntropyLoss
-                    y_target[0][final_word_indices[batch_input_count]] = 1 
+                    y_target[0][final_word_indices[batch_input_count]] = 1
                     # Calc CrossEntropyLoss
-                    loss = loss_func(output_logits.float(), y_target.float()) # calculates loss 
-                    #if int(loss.item()) >= 600:
-                        #large_losses += 1
-                        #print(f"The number of large losses is: {large_losses}")
+                    loss = loss_func(output_logits.float(), y_target.float()) # calculates loss
                     # Add to losses array
                     losses[i] = loss.item()
-                    # Check if 100,000 examples have been processed
-                    if i % 100000 == 0 and batch_input_count == 19:
-                        # If so, print stats
-                        #sub_arr = torch.tensor(losses[0:i].tolist())
-                        curr_loss_avg = torch.mean(losses[0:i])
-                        print(f"Loop Iteration: {i} out of {len(data_loader.unlabeled_seqs)}\n")
-                        print(f"The loss of this of this iteration  is: {losses[i]}\n")
-                        print(f"The average loss so far is: {curr_loss_avg}")
-                        print(f"The time for this batch: {time.time() - batch_timer}")
-                        #curr_loss_avg_tensor = torch.tensor(curr_loss_avg, device=device, dtype=torch.float64)
-                        print(f"The perplexity of this model (so far) is: {torch.exp(curr_loss_avg)}")
-                        #pdb.set_trace()
 
                 # Update After Batch of 20
                 optimizer.zero_grad()
@@ -167,16 +77,39 @@ def _train(model, data_loader, optimizer, device=torch.device("cuda:0")):
                 optimizer.step()
                 # Reset Tensors
                 final_word_indices = torch.zeros(20, device=device).long()
-                y_target = torch.zeros(1, len(data_loader.vocab), device=device).long() 
+                y_target = torch.zeros(1, len(data_loader.vocab), device=device).long()
                 model_input_tensor = torch.zeros((20, 5), device=device).long()
 
-            # Return Early
-            #if i == 300000:
-                #return model
-        print(f"\nTRAINING EPOCH-{count + 1} FINISHED") 
+            if i == len(data_loader) - 1:
+                training_perp[count] = torch.exp(torch.mean(losses[0:i]))
+                valid_perp[count] = _test(model, valid_loader, data_loader, optimizer)
+                
+        print(f"\nTRAINING EPOCH-{count + 1} FINISHED")
         print(f"The time for this epoch: {time.time() - epoch_timer}\n")
         # Incr Epoch Count
         count += 1
+
+    # Make Plots - Train
+    plt.figure(figsize=(10, 10))
+    plt.plot(training_perp.squeeze().tolist())
+    plt.xlabel("Epoch")
+    plt.ylabel("Perplexity")
+    plt.title("Training Perplexities")
+    plt.savefig("training.png")
+
+    # Make Plots - Valid    
+    plt.figure(figsize=(10, 10))
+    plt.plot(valid_perp.squeeze().tolist())
+    plt.xlabel("Epoch")
+    plt.ylabel("Perplexity")
+    plt.title("Validation Perplexities")
+    plt.savefig("valid.png")
+    
+    # Print 
+    print("Final Training Perplexity", training_perp[-1])
+    print("Final Validation Perplexity", valid_perp[-1])
+    
+    # Return
     print("\nTRAINING FINISHED\n")
     return model
 
@@ -184,83 +117,93 @@ def _test(model, data_loader, train_loader, optimizer, device=torch.device("cuda
     """
     This function will evaluate a trained neural network on a validation
     set or testing set
-
     Returns accuracy
     """
     print("\nTESTING MODEL\n")
+    
+    # Init
     loss_func = nn.CrossEntropyLoss().cuda(0)
-    losses = torch.zeros(len(data_loader), dtype=torch.float64, device=device)#np.empty(shape=len(data_loader), dtype='f')
+    losses = torch.zeros(len(data_loader), dtype=torch.float64, device=device)
     model_input_tensor = torch.zeros((20, 5), device=device).long()
     final_word_indices = torch.zeros(20, device=device).long()
     y_target = torch.zeros(1, len(train_loader.vocab), device=device).long()
+    
+    # Loop through Test Data
     for i, data in enumerate(data_loader):
         # Batch Idx
         batch_idx = i % 20
         # Get context and ground truth tokens
         context, final_word = data
+
+        # Loop through context
         for idx, word in enumerate(context):
             # Check if word out of vocab
             if word not in train_loader.vocab:
                 # UNK it
                 word = "<unk>"
-            # Get embedding  & add to context tensor 
-            #word_embedding = model.embeddings.weight[train_loader.vocab2index[word]]
-            #context_tensor_row[idx,:] = word_embedding
             model_input_tensor[batch_idx][idx] = train_loader.vocab2index[word]
+
         # Check for words unknown to model
         if final_word not in train_loader.vocab:
             # If exist, UNK them
-            final_word = "<unk>" 
+            final_word = "<unk>"
         # Assign to context_tensor
         final_word_indices[batch_idx] = train_loader.vocab2index[final_word]
+
         # Check if we have a 20 batch
         if i % 20 == 0 and i != 0:
             for batch_input_count in range(20):
-                #batch_context_tensor = context_tensor[batch_input_count].flatten()
-                #output_logits = model(batch_context_tensor) # run the forward pass and get a prediction
                 # Get Model Prediction
                 output_logits, log_probs = model(model_input_tensor[batch_input_count]) # run the forward pass and get a prediction
                 output_logits = torch.reshape(output_logits, (1, len(train_loader.vocab)))
                 final_word_idx = train_loader.vocab2index[final_word]
                 y_target[0][final_word_indices[batch_input_count]] = 1
-                # Calc loss for each 
+
+                # Calc loss for each
                 loss = loss_func(output_logits.float(), y_target.float()) # calculates loss
                 losses[i] = loss.item()
+
             # Reset Context Tensors
-            #context_tensor = torch.zeros((20, 500), device=device)
-            #context_tensor_row = torch.zeros((5, 100), device=device)
             final_word_indices = torch.zeros(20, device=device).long()
             y_target = torch.zeros(1, len(train_loader.vocab), device=device).long()
             model_input_tensor = torch.zeros((20, 5), device=device).long()
     
+    # Print Finished Msg
     print("\nFINISHED TESTING MODEL\n")
     avg_loss = torch.mean(losses)
-    #avg_loss_tensor = torch.tensor(avg_loss, device=device, dtype=torch.float64)
     perplexity = torch.exp(avg_loss)
-    print(f"The perplexity is {perplexity}")
-    print(f"The average loss is {avg_loss}")
-    print(f"The first 50 losses are {losses[0:50]}")
+    print(f"The final perplexity is {perplexity}")
+    # Return
+    return perplexity
 
-    return avg_loss, perplexity
-    
 
 if __name__ == "__main__":
     # PyTorch Inits
     torch.cuda.init() # Cuda
-    gc.collect()      # Garbage Collection
     torch.cuda.empty_cache() # Empty Mem Cache
+
     # Load data_loaders from pickle files
     train_dataloader = pickle.load(open('train_dataloader.p', 'rb'))
     test_dataloader = pickle.load(open('test_dataloader.p', 'rb'))
+    valid_dataloader = pickle.load(open('valid_dataloader.p', 'rb'))
+    
+    # Get Model
     train_model =  Feed_Forward(len(train_dataloader.vocab))
+
     # Check for cuda initialization and cuda device
     print(f"initialized? {torch.cuda.is_initialized()}")
     print(f"device name: {torch.cuda.get_device_name(0)}")
-    train_model = train_model.cuda(0) # Move model to gpu
-    print(f"model param: {next(train_model.parameters()).device}")
+     
+    # Move model to gpu 
+    train_model = train_model.cuda(0) 
     # optimizer for model
     test_optimizer = optim.SGD(train_model.parameters(), lr=0.0001)
-    train_model.train() # Set model to training model
-    _train(train_model, train_dataloader, test_optimizer) # train
-    train_model.eval() # Set model to eval mode
+
+    # Train & Test 
+    train_model.train() # Set model to training model 
+    _train(train_model, train_dataloader, valid_dataloader, test_optimizer) # train
+
+    train_model.eval() # Set model to eval mode 
     _test(train_model, test_dataloader, train_dataloader,  test_optimizer) # Test
+
+ 
